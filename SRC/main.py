89eda3e2bdeecf0b57e15a55ff3d1b6e1cc66d10
kia-8 <kia.zarenasr@gmail.com>
2026-05-03 15:04:56 +0200

    CV2 is initialized and works!

--- SRC/main.py
+++ SRC/main.py
@@ -1,5 +1,25 @@
-print("Hello World!")
-a = 5
-b = 10
-c = a + b
-print(c)
+import cv2
+import sys
+import time
+
+cap = cv2.VideoCapture(0)
+frame_count = 0
+prev_time = time.time()
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    current_time = time.time()
+    fps = 1 / (current_time - prev_time)
+    prev_time = current_time
+
+    cv2.imshow('Frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release() 
+cv2.destroyAllWindows()
